98aa13031af4c8411d45ccafc96fdf971ddf02c3
santhosh0209 <bluyblack0902@gmail.com>
2020-04-09 12:59:52 +0530

    initial commit

--- Bloger/settings.py
+++ Bloger/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Bloger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
