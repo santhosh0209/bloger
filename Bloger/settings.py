63e99ccbd112605f17a00874b46f97d2d5571a06
santhosh0209 <bluyblack0902@gmail.com>
2020-04-09 12:41:36 +0530

    initial commit

--- Bloger/settings.py
+++ Bloger/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Bloger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
